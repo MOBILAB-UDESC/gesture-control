7cbf92738fbe9e35e2954eb8449971d50a15b556
Douglas Bertol <douglas.bertol@udesc.br>
2024-08-28 16:41:11 -0300

    changing all the code

--- uniao-face-coltrol.py
+++ uniao-face-coltrol.py
@@ -51,85 +51,108 @@ hands_detected = False
 admin_in_the_house = False
 read = False
 fps = 0
+people = []
 
 
 def detect():
-    global left_hand_center, right_hand_center, center_face, hands_detected, admin_in_the_house, frame, read, fps
+    global left_hand_center, right_hand_center, center_face, hands_detected
+    global people, admin_in_the_house, frame, read, fps
 
     both_hands_detect = 0
     fps = 0
-    image = frame
-
     # Initialize face and hand detectors
-    with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as face_detector, \
-            mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5) as hands_detector:
+    hands_detector = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
 
-        while admin_in_the_house:
-            if not read:
-                continue
+    while True:
+        if not read:
+            continue
 
-            # frame = cv.flip(np.asanyarray(color_frame.get_data()), 1)
-            rgb_frame = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        image = frame # para garantir que a imagem não seja atualizada durante o processamento
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        boxes = face_recognition.face_locations(rgb, model="hog")
+        encodings = face_recognition.face_encodings(rgb, boxes)
 
-            # Detect faces
-            face_results = face_detector.process(rgb_frame)
-            frame_height, frame_width, _ = image.shape
+        # Initialize the list of names for each face detected
+        people = []
 
-            # Detect hands
-            hand_results = hands_detector.process(rgb_frame)
+        j = 0
+        # Loop over the facial embeddings
+        for encoding in encodings:
+            matches = face_recognition.compare_faces(data["encodings"], encoding)
+            name = "Unknown"
+            if True in matches:
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
+                name = max(counts, key=counts.get)
 
-            if face_results.detections:
-                for face in face_results.detections:
-                    face_rect = np.multiply(
-                        [
-                            face.location_data.relative_bounding_box.xmin,
-                            face.location_data.relative_bounding_box.ymin,
-                            face.location_data.relative_bounding_box.width,
-                            face.location_data.relative_bounding_box.height,
-                        ],
-                        [frame_width, frame_height, frame_width, frame_height]
-                    ).astype(int)
+            people.append([name, boxes[j]])
+            j = j+1
 
-                    key_points = np.array([(p.x, p.y) for p in face.location_data.relative_keypoints])
-                    key_points_coords = np.multiply(key_points, [frame_width, frame_height]).astype(int)
+        #     # frame = cv.flip(np.asanyarray(color_frame.get_data()), 1)
+        #     rgb_frame = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-                    # Calculate center of the face
-                    center_face = (int(face_rect[0] + face_rect[2] / 2), int(face_rect[1] + face_rect[3] / 2))
+        #     # Detect faces
+        #     face_results = face_detector.process(rgb_frame)
+        #     frame_height, frame_width, _ = image.shape
 
-                    # Draw key points on face
-                    for x, y in key_points_coords:
-                        cv.circle(image, (x, y), 5, (255, 0, 255), -1)
-                    cv.circle(image, (center_face[0], center_face[1]), 5, (255, 0, 0), -1)
+        #     # Detect hands
+        #     hand_results = hands_detector.process(rgb_frame)
 
-            if hand_results.multi_hand_landmarks and len(hand_results.multi_hand_landmarks) == 2:
-                hands_detected = True
+        #     if face_results.detections:
+        #         for face in face_results.detections:
+        #             face_rect = np.multiply(
+        #                 [
+        #                     face.location_data.relative_bounding_box.xmin,
+        #                     face.location_data.relative_bounding_box.ymin,
+        #                     face.location_data.relative_bounding_box.width,
+        #                     face.location_data.relative_bounding_box.height,
+        #                 ],
+        #                 [frame_width, frame_height, frame_width, frame_height]
+        #             ).astype(int)
 
-                for i, hand_landmarks in enumerate(hand_results.multi_hand_landmarks):
-                    hand_points = []
+        #             key_points = np.array([(p.x, p.y) for p in face.location_data.relative_keypoints])
+        #             key_points_coords = np.multiply(key_points, [frame_width, frame_height]).astype(int)
 
-                    for landmark in hand_landmarks.landmark:
-                        x = int(landmark.x * frame_width)
-                        y = int(landmark.y * frame_height)
-                        hand_points.append((x, y))
-                        cv.circle(image, (x, y), 5, (255, 0, 255), cv.FILLED)
+        #             # Calculate center of the face
+        #             center_face = (int(face_rect[0] + face_rect[2] / 2), int(face_rect[1] + face_rect[3] / 2))
 
-                    # Calculate left hand center
-                    if hand_results.multi_handedness[i].classification[0].label == "Left":
-                        left_hand_center = tuple(np.mean(hand_points, axis=0).astype(int))
-                        cv.circle(image, (left_hand_center[0], left_hand_center[1]), 5, (255, 0, 0), cv.FILLED)
+        #             # Draw key points on face
+        #             for x, y in key_points_coords:
+        #                 cv.circle(image, (x, y), 5, (255, 0, 255), -1)
+        #             cv.circle(image, (center_face[0], center_face[1]), 5, (255, 0, 0), -1)
 
-                    # Calculate right hand center
-                    if hand_results.multi_handedness[i].classification[0].label == "Right":
-                        right_hand_center = tuple(np.mean(hand_points, axis=0).astype(int))
-                        cv.circle(image, (right_hand_center[0], right_hand_center[1]), 5, (255, 0, 0), cv.FILLED)
-            else:
-                hands_detected = False
+        #     if hand_results.multi_hand_landmarks and len(hand_results.multi_hand_landmarks) == 2:
+        #         hands_detected = True
 
-            cv.putText(image, f"FPS: {fps:.2f}", (30, 30), cv.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 255), 2)
-            cv.imshow("frame", image)
-            key = cv.waitKey(1)
+        #         for i, hand_landmarks in enumerate(hand_results.multi_hand_landmarks):
+        #             hand_points = []
 
-        cv.destroyAllWindows()
+        #             for landmark in hand_landmarks.landmark:
+        #                 x = int(landmark.x * frame_width)
+        #                 y = int(landmark.y * frame_height)
+        #                 hand_points.append((x, y))
+        #                 cv.circle(image, (x, y), 5, (255, 0, 255), cv.FILLED)
+
+        #             # Calculate left hand center
+        #             if hand_results.multi_handedness[i].classification[0].label == "Left":
+        #                 left_hand_center = tuple(np.mean(hand_points, axis=0).astype(int))
+        #                 cv.circle(image, (left_hand_center[0], left_hand_center[1]), 5, (255, 0, 0), cv.FILLED)
+
+        #             # Calculate right hand center
+        #             if hand_results.multi_handedness[i].classification[0].label == "Right":
+        #                 right_hand_center = tuple(np.mean(hand_points, axis=0).astype(int))
+        #                 cv.circle(image, (right_hand_center[0], right_hand_center[1]), 5, (255, 0, 0), cv.FILLED)
+        #     else:
+        #         hands_detected = False
+
+        #     cv.putText(image, f"FPS: {fps:.2f}", (30, 30), cv.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 255), 2)
+        #     cv.imshow("frame", image)
+        #     key = cv.waitKey(1)
+
+        # cv.destroyAllWindows()
 
 
 def control():
@@ -182,54 +205,42 @@ def capture():
 # Create and start captureThread
 captureThread = threading.Thread(target=capture)
 captureThread.start()
+
+# Create and start detectThread
+camThread = threading.Thread(target=detect)
+camThread.start()
+
 control_started = False
+admins = ['deborah', 'douglas']
 
 while True:
     if not read:
         continue
 
-    image = frame # para garantir que a imagem não seja atualizada durante o processamento
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    boxes = face_recognition.face_locations(rgb, model="hog")
-    encodings = face_recognition.face_encodings(rgb, boxes)
-
+    image = frame
     if not admin_in_the_house:
-        for (y1, x1, y2, x2) in boxes:
-            # Desenhando retangulo da face
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        for person in people:
+            for name, (y1, x1, y2, x2) in people:
+                # Desenhando retangulo da face
+                cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.putText(image, name, (x2, y2+20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
         
+        cv2.putText(image, f"FPS: {fps:.2f}", (30, 30), cv.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 255), 2)
         cv2.imshow("frame", image) 
         cv2.waitKey(1)        
 
-    # Initialize the list of names for each face detected
-    names = []
 
-    # Loop over the facial embeddings
-    for encoding in encodings:
-        matches = face_recognition.compare_faces(data["encodings"], encoding)
-        name = "Unknown"
-        if True in matches:
-            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-            counts = {}
-            for i in matchedIdxs:
-                name = data["names"][i]
-                counts[name] = counts.get(name, 0) + 1
-            name = max(counts, key=counts.get)
+    # for name in names:
+    #     if name in admins: 
+    #         if not admin_in_the_house:
+    #             print('ACHEI' + name)
+    #             admin_in_the_house = True
+    #             break
+    #     else:
+    #         print('Corram o Admin sumiu!!!!')
+    #         admin_in_the_house = False
 
-        names.append(name)
-
-    for name in names:
-        if name == 'deborah' and not admin_in_the_house:
-            print('ACHEI A DEBORAH')
-            admin_in_the_house = True
-            camThread = threading.Thread(target=detect)
-            camThread.start()
-            break
-        elif name != 'deborah':
-            print('não é a Deborah')
-            admin_in_the_house = False
-
-    if hands_detected and not control_started:
-        controlThread = threading.Thread(target=control)
-        controlThread.start()
-        control_started = True
+    # if hands_detected and not control_started:
+    #     controlThread = threading.Thread(target=control)
+    #     controlThread.start()
+    #     control_started = True
